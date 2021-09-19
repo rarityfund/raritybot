df8b7968144aae8e7e43334d16f1c3ee9d3006de
Antoine Sachet <antoine.sac@gmail.com>
2021-09-19 17:35:00 +0100

    Add command as positional CLI arg
    This will enable more fonctionalities such as summoning, crafting, etc

--- rarity.py
+++ rarity.py
@@ -10,35 +10,52 @@ from list_summoners import list_summoners
 DEFAULT_KEY_FILE = "privatekeyencrypted.json"
 
 def print_intro():
-    print(Fore.RED + 'Welcome to Rarity bot V1.1\n')
-    print("This script will:")
-    print("- Send your summoners on adventures, if they're ready")
-    print("- Level up your summoners when it's time")
-    print("- Claim gold on their behalf if any is available")
-    print("- Send your summoners to the Cellar dungeon, if they beat it")
-    print("\n")
+    print(Fore.RED + 'Welcome to Rarity bot V1.1')
 
-
-if (__name__ == "__main__"):
-
-    # Parsing CLI args
+def create_parser():
     parser = argparse.ArgumentParser(description='Manage your rarity summoners')
+    # Main command to direct what we do for that session
+    parser.add_argument("command", default = "run", choices = ["run", "list", "summon", "check_gas"],
+                        help='''Main command:
+                        "run" to run the bot (configure with --actions),
+                        "list" to simply list summoners,
+                        "summon" to create new summoners (configure with -n and --class)
+                        "check_gas" to show price in FTM of common actions,
+                        ''',
+                        ) 
+
+    # Authentication-related arguments: keyfile, password, import-key
     parser.add_argument('-k', '--keyfile', help='Path to encrypted keyfile', 
                         default = DEFAULT_KEY_FILE)
     parser.add_argument('-p', '--password', help='''Password to decrypt the keyfile. 
                         Be aware it will be available in plaintext in the shell history. 
                         Use of interactive login is preferable if possible.''', default = '')
-    parser.add_argument('-i', '--import-key', help='''Import a private key which will be stored 
-                        encrypted in `privatekeyencrypted.json`)''', 
-                        action = 'store_const', const = True, default = False)
-    parser.add_argument('-a', '--actions', help='''All actions to take. Will do everything by default.
-                                                   Select one or more from 
-                                                   "list", "adventure", "level_up", "claim_gold", "cellar", "check_gas"''',
-                        nargs='*', default = ["list", "adventure", "level_up", "claim_gold", "cellar"])
-    parser.add_argument('-t', '--txmode', help='''How transactions are processed. 
+    parser.add_argument('--import-key', help='''Import a private key which will be stored 
+                        encrypted in `privatekeyencrypted.json`''', 
+                        action = 'store_true')
+
+    # General config arguments: txmode
+    parser.add_argument('--txmode', help='''How transactions are processed. 
                                             "legacy" to send them one by one and wait for the receipt each time.
                                             "batch" to send tx in batches and wait less often''',
                         default = "legacy")
+    
+    # Command RUN takes argument --actions:
+    parser.add_argument('-a', '--actions', help='''All actions to take. Will do everything by default.
+                                                   Select one or more from 
+                                                   "list" (list Summoners on address), 
+                                                   "adventure", 
+                                                   "level_up", 
+                                                   "claim_gold", 
+                                                   "cellar" (send to cellar dungeon)''',
+                        nargs='*', default = ["list", "adventure", "level_up", "claim_gold", "cellar"])
+
+    return parser
+
+if (__name__ == "__main__"):
+
+    # Parsing CLI args
+    parser = create_parser()
     args = parser.parse_args()
 
     print_intro()
@@ -60,7 +77,6 @@ if (__name__ == "__main__"):
         """)
         exit()
            
-    
     # Load account details from keyfile
     try:
         if args.password != '':
@@ -79,54 +95,57 @@ if (__name__ == "__main__"):
     # The transacter will handle the signing and executing of transactions
     transacter = Transacter(owner_address, private_key, txmode = args.txmode)
 
-
-    if "check_gas" in args.actions:
+    if args.command == "check_gas":
         transacter.print_gas_price()
+    elif args.command == "summon":
+        print("Not implemented")
+        pass
+    elif args.command == "list":
+        list_summoners(owner_address, transacter, verbose = True)
+    elif args.command == "run":
+        print("Scanning for summoners...\n")
+        print_list = "list" in args.actions
+        summoners = list_summoners(owner_address, transacter, verbose = print_list)
+        print("\n")
 
-    print("Scanning for summoners...\n")
-    print_list = "list" in args.actions
-    summoners = list_summoners(owner_address, transacter, verbose = print_list)
-    print("\n")
+        if not summoners:
+            print("This address doesn't contains any rarities, bot is exiting...")
+            exit()
 
-    if not summoners:
-        print("This address doesn't contains any rarities, bot is exiting...")
-        exit()
+        print("Looking for things to do ...")
 
-    print("Looking for things to do ...")
+        # We process each action entirely before moving to the next one
+        # This is important as, this way, we can wait for tx status only once at the end of a batch
+        # This way, level_up will "see" the correct XP and claim_gold will "see" the correct level
 
-    # We process each action entirely before moving to the next one
-    # This is important as, this way, we can wait for tx status only once at the end of a batch
-    # This way, level_up will "see" the correct XP and claim_gold will "see" the correct level
+        if "adventure" in args.actions:
+            print("Checking adventures...")
+            for summoner in summoners:        
+                summoner.adventure()
+            transacter.wait_for_pending_transations()
 
-    if "adventure" in args.actions:
-        print("Checking adventures...")
-        for summoner in summoners:        
-            summoner.adventure()
-        transacter.wait_for_pending_transations()
+        if "level_up" in args.actions:
+            print("Checking level-up...")
+            for summoner in summoners:
+                summoner.level_up()
+            transacter.wait_for_pending_transations()
+        
+        if "claim_gold" in args.actions:
+            print("Checking gold claims...")
+            for summoner in summoners:
+                summoner.claim_gold()
+            transacter.wait_for_pending_transations()
 
-    if "level_up" in args.actions:
-        print("Checking level-up...")
-        for summoner in summoners:
-            summoner.level_up()
-        transacter.wait_for_pending_transations()
-    
-    if "claim_gold" in args.actions:
-        print("Checking gold claims...")
-        for summoner in summoners:
-            summoner.claim_gold()
-        transacter.wait_for_pending_transations()
-
-    if "cellar" in args.actions:
-        print("Checking cellar loot...")
-        for summoner in summoners:
-            summoner.go_cellar()
-        transacter.wait_for_pending_transations()
-    
-    # Wait for all tx to complete
+        if "cellar" in args.actions:
+            print("Checking cellar loot...")
+            for summoner in summoners:
+                summoner.go_cellar()
+            transacter.wait_for_pending_transations()
+    else:
+        print(Fore.RED + "Unrecognised command")    
+        
+    # Wait for all tx to complete (just in case!)
     transacter.wait_for_pending_transations()
-
-    print("\n")
-    print(Fore.RED + "Our tasks are done now, time to rest, goodbye")
-    print(Fore.RED + "Total session cost: " + str(round(transacter.session_cost, 6)) + " FTM")
+    print("\n" + Fore.RED + "Total session cost: " + str(round(transacter.session_cost, 6)) + " FTM")
     
 
